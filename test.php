2f1a67847d228b0b2978b339edbfb66ac45b4d16
aymanabueideh <ayman.h.97abu-eideh@hotmail.com>
2018-03-20 23:02:38 +0200

    ayman

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ayman
+ * Date: 3/20/2018
+ * Time: 9:36 PM
+ */
+
+$DataBase=new mysqli('localhost','root','','gamesguideproject') or die('Unable to connect');
+
+echo 'connected and connected';
+
+?>
